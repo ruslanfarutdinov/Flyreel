318667f2194ce86eedec6b494d26ff9060162f45
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2021-06-19 11:35:27 -0700

    Create initial PieChart component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import PieChart from './components/PieChart/PieChart';
+
+import { data } from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PieChart 
+        pieData={data} 
+        width={500} 
+        height={500} />
     </div>
   );
 }
---
+++ src/components/PieChart/PieChart.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+function PieChart({ 
+  pieData, 
+  width, 
+  height, 
+}) {
+  const ref = useRef(null);
+
+  const createPie = d3
+    .pie()
+    .value(d => d.value)
+    .sort(null);
+
+  const createArc = d3
+    .arc()
+    .innerRadius(0)
+    .outerRadius(Math.min(width, height) / 2 - 1);
+  
+  const color = d3
+    .scaleOrdinal()
+    .domain(pieData.map(d => d.name))
+    .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), pieData.length)
+    .reverse());
+  
+  const arcLabel = () => {
+    const radius = Math.min(width, height) / 2 * 0.8;
+    return d3.arc().innerRadius(radius).outerRadius(radius);
+  };
+
+  useEffect(
+    () => {
+      const arcs = createPie(pieData);
+
+      const svg = d3.select(ref.current)
+        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+      svg.append("g")
+        .attr("stroke", "white")
+        .selectAll("path")
+        .data(arcs)
+        .join("path")
+        .attr("fill", d => color(d.data.name))
+        .attr("d", createArc)
+        .append("title")
+        .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
+
+      svg.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 12)
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(arcs)
+        .join("text")
+        .attr("transform", d => `translate(${arcLabel().centroid(d)})`)
+        .call(text => text.append("tspan")
+        .attr("y", "-0.4em")
+        .attr("font-weight", "bold")
+        .text(d => d.data.name))
+        .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+        .attr("x", 0)
+        .attr("y", "0.7em")
+        .attr("fill-opacity", 0.7)
+        .text(d => d.data.value.toLocaleString()));
+    },
+    [pieData]
+  );
+
+  return (
+    <svg width={width} height={height} ref={ref}></svg>
+  );
+}
+
+export default PieChart;
