1f24cd498cfc96462d8dfacfb8ed1f3c88a965c6
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2021-06-20 11:43:27 -0700

    Implement Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import PieChart from './components/PieChart/PieChart';
 import BarChart from './components/BarChart/BarChart';
+import Dropdown from './components/Dropdown/Dropdown';
 
 import './App.css';
 import { populationData } from './data';
 
 function App() {
+  const [dropdownOptions] = useState([
+    { id: 1, value: 'Pie Chart' },
+    { id: 2, value: 'Bar Chart' },
+  ]);
+
+  const onDropdownChangeHandler = useCallback(e => {
+    e.preventDefault();
+    console.log(e.target.value);
+  });
+
   return (
     <div className="App">
       <PieChart 
@@ -19,6 +30,12 @@ function App() {
         width={700}
         height={500}
       /> */}
+      <Dropdown 
+        selectId="population"
+        labelText="Choose chart type:"
+        options={dropdownOptions}
+        onChangeHandler={onDropdownChangeHandler}
+      />
     </div>
   );
 }
---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Dropdown.css';
+
+function Dropdown({ 
+  selectId,
+  labelText,
+  options,
+  onChangeHandler,
+ }) {
+  return (
+    <div className="Dropdown">
+      <label htmlFor={selectId}>{labelText}</label>
+      <select id={selectId} onChange={onChangeHandler}>
+        <option value="">--Please choose an option--</option>
+        {options.map(option => (
+          <option key={option.id} value={option.value}>{option.value}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+Dropdown.propTypes = {
+  selectId: PropTypes.string.isRequired,
+  labelText: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      value: PropTypes.string,
+    })).isRequired,
+  onChangeHandler: PropTypes.func.isRequired,
+};
+
+export default Dropdown;
